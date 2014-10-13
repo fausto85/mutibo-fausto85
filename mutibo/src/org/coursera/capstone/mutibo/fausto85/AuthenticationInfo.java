9f77bcf36015c452c7d41a960ba5f8518354b8ca
Fausto Romano <fausto85@gmail.com>
2014-10-12 22:52:21 -0500

    First commit. Initial Login Activity & AuthenticationManager

---
+++ mutibo/src/org/coursera/capstone/mutibo/fausto85/AuthenticationInfo.java
@@ -0,0 +1,36 @@
+package org.coursera.capstone.mutibo.fausto85;
+
+public class AuthenticationInfo {
+
+	private String mUser;
+
+	public String getUser() {
+		return mUser;
+	}
+
+	public void setUser(String user) {
+		this.mUser = user;
+	}
+
+	public String getPassword() {
+		return mPassword;
+	}
+
+	public void setPassword(String password) {
+		this.mPassword = password;
+	}
+
+	private String mPassword;
+	
+	private Boolean mUserAuthenticated;
+
+	public Boolean getUserAuthenticated() {
+		return mUserAuthenticated;
+	}
+
+	public void setUserAuthenticated(Boolean userAuthenticated) {
+		this.mUserAuthenticated = userAuthenticated;
+	}
+	
+	
+}
---
+++ mutibo/src/org/coursera/capstone/mutibo/fausto85/AuthenticationManager.java
@@ -0,0 +1,53 @@
+package org.coursera.capstone.mutibo.fausto85;
+
+import java.util.ArrayList;
+
+public class AuthenticationManager {
+
+	//TODO: Only one account should be ever present
+	private ArrayList<AuthenticationInfo> mAccounts;
+	//private AuthenticationInfo mAccount;
+
+	/**
+	 * A dummy authentication store containing known user names and passwords.
+	 * TODO: remove after connecting to a real authentication system.
+	 */
+	private static final String[] DUMMY_CREDENTIALS = new String[] {
+		"foo@example.com", "12345678" };
+
+	private static AuthenticationManager instance = null;
+	
+	public Boolean isUserRegistered(AuthenticationInfo account){
+		//TODO: this should ask a web service
+		Boolean registered = false;
+		for ( AuthenticationInfo a : mAccounts ){
+			if(a.getUser().equals(account.getUser()))
+				registered = true;
+		}
+		return registered;
+	}
+
+	public Boolean registerUser(AuthenticationInfo account){
+		Boolean registered = false;
+		//TODO: this should send the information to a web service
+		mAccounts.add(account);
+		registered = true;
+		return registered;
+	}
+	
+	public static AuthenticationManager getInstance(){
+		if(instance == null){
+			instance = new AuthenticationManager();
+		}
+		return instance;
+	}
+	
+	protected AuthenticationManager(){
+		//Singleton
+		AuthenticationInfo account =  new AuthenticationInfo();
+		mAccounts = new ArrayList<AuthenticationInfo>();
+		account.setUser(DUMMY_CREDENTIALS[0]);
+		account.setPassword(DUMMY_CREDENTIALS[1]);
+		mAccounts.add(account);
+	}
+}
